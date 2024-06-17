64f6bd9f3bc967d9058549286bdd5d0181113915
Cinquain <James@cityXcape.com>
2024-06-17 11:07:23 -0500

    Initial Commit

---
+++ Circular Carousal/Circular_CarousalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circular_CarousalApp.swift
+//  Circular Carousal
+//
+//  Created by James Allan on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Circular_CarousalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
